36c010f55db4897f8132d14d0d2e0930eefd5d43
Aditya S Rao <aditya.s.rao12@gmail.com>
2022-08-13 21:06:30 +0530

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { Layout } from '../components/layout/Layout';
+
+const Index = () => {
+  return (
+    <Layout>
+      <h1>My landing page</h1>
+      <p>This is my landing page</p>
+      <Link to='/about'>About Me</Link>
+    </Layout>
+  );
+};
+
+export default Index;
