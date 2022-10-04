260dbf647c9f220b47ea8690b15135d8b4a654c9
Aditya S Rao <aditya.s.rao12@gmail.com>
2022-10-04 23:30:25 +0530

    Added background color.

--- src/pages/contact-me/index.tsx
+++ src/pages/contact-me/index.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { PageContainer } from '../../components/layout/container/PageContainer';
+import { ThemeContext } from '../../context/ThemeContext';
 
 interface ContactMeInterface {}
 
@@ -11,13 +12,63 @@ const css_prefix = 'c-m__';
 interface ContactMeInterface {}
 
 const ContactMe: FC<ContactMeInterface> = () => {
+  const { theme } = React.useContext(ThemeContext);
+
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState<string>();
+  const [email, setEmail] = useState<string>('');
+
+  const onChangeFirstName = (payload: string) => {
+    setFirstName(payload);
+  };
+
+  const onChangeLastName = (payload: string) => {
+    setLastName(payload);
+  };
+
+  const onChangePhoneNumber = (payload: string) => {
+    setPhoneNumber(payload);
+  };
+
+  const onChangeEmail = (payload: string) => {
+    setEmail(payload);
+  };
+
+  const _renderInputComponent = (
+    title: string,
+    value: string,
+    onChange: (payload: string) => void
+  ) => {
+    return (
+      <div className={`${css_prefix}input-main`}>
+        <div className={`${css_prefix}input-title`}>{title}</div>
+
+        <div className={`${css_prefix}input-value`}>
+          <input
+            placeholder='Name'
+            value={value}
+            onChange={({ currentTarget }) => onChange(currentTarget.value)}
+            type='text'
+          />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <PageContainer>
-      <div className={`${css_prefix}main`}>
+      <div
+        className={`${css_prefix}main ${
+          theme === 'dark' ? css_prefix + 'main-dark' : ''
+        }`}
+      >
         <div className={`${css_prefix}container`}>
-          <div>Name</div>
+          <div className={`${css_prefix}container-heading`}>Contact Me</div>
 
-          <div>EMail</div>
+          <div className={`${css_prefix}container-line-1`}>
+            {_renderInputComponent('Name', firstName, onChangeFirstName)}
+          </div>
         </div>
       </div>
     </PageContainer>
