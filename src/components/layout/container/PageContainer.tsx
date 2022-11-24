55e0e65e8c3f1b4dba8a37903c3b7c65c7dc0572
Aditya S Rao <aditya.s.rao12@gmail.com>
2022-11-24 09:06:37 +0530

    Added media query for 1500px.

--- src/components/layout/container/PageContainer.tsx
+++ src/components/layout/container/PageContainer.tsx
@@ -1,18 +1,18 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment } from "react";
 
-import { Header } from '../header/Header';
-import { Footer } from '../footer/Footer';
-import ThemeProvider, { ThemeContext } from '../../../context/ThemeContext';
-import { Scrollbars } from 'react-custom-scrollbars-2';
+import { Header } from "../header/Header";
+import { Footer } from "../footer/Footer";
+import ThemeProvider, { ThemeContext } from "../../../context/ThemeContext";
+import { Scrollbars } from "react-custom-scrollbars-2";
 
-import './PageContainer.scss';
+import "./PageContainer.scss";
 
 interface PageContainerProps {
   children: JSX.Element;
 }
 
 // Components -- layout -- container -- page - container
-const css_prefix = 'c--l--c--p-c__';
+const css_prefix = "c--l--c--p-c__";
 
 export const PageContainer: FC<PageContainerProps> = ({ children }) => {
   const { theme, switchTheme } = React.useContext(ThemeContext);
@@ -21,9 +21,9 @@ export const PageContainer: FC<PageContainerProps> = ({ children }) => {
     <ThemeProvider>
       <main
         className={`${css_prefix}main ${
-          theme === 'dark'
-            ? css_prefix + 'main-dark'
-            : css_prefix + 'main-light'
+          theme === "dark"
+            ? css_prefix + "main-dark"
+            : css_prefix + "main-light"
         }`}
       >
         <Header theme={theme} switchTheme={switchTheme} />
