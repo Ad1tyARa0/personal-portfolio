70c560b55b3ff407c08d0097fd3af0dde675917c
Aditya S Rao <aditya.s.rao12@gmail.com>
2023-01-02 16:45:26 +0530

    Updated footer.

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,11 +1,13 @@
-import React, { FC } from 'react';
-import { gatsbyLogo } from '../../../assets/svg';
+import React, { FC } from "react";
+import { gatsbyLogo } from "../../../assets/svg";
+import { SiTypescript } from "react-icons/si";
+import { AiOutlineCopyrightCircle } from "react-icons/ai";
 
 // SCSS.
-import './Footer.scss';
+import "./Footer.scss";
 
 // Components -- layout -- footer
-const css_prefix = 'c--l--f__';
+const css_prefix = "c--l--f__";
 
 // Component props.
 interface FooterProps {
@@ -16,14 +18,28 @@ const FooterComponent: FC<FooterProps> = ({ theme }) => {
   return (
     <footer
       className={`${css_prefix}main ${
-        theme === 'dark' ? css_prefix + 'main-dark' : ''
+        theme === "dark" ? css_prefix + "main-dark" : ""
       }`}
     >
       <div className={`${css_prefix}body`}>
+        <div className={`${css_prefix}copyright-container`}>
+          <div className={`${css_prefix}copyright-icon`}>
+            <AiOutlineCopyrightCircle />
+          </div>
+
+          <div className={`${css_prefix}copyright-text`}>
+            2022 Developer Portoflio. All Rights Reserved
+          </div>
+        </div>
+
         <div className={`${css_prefix}text`}>Built With</div>
 
         <div className={`${css_prefix}body-logo`}>
-          <img src={gatsbyLogo} alt='Gatsby' />
+          <img src={gatsbyLogo} alt="Gatsby" />
+        </div>
+
+        <div className={`${css_prefix}typescript-logo`}>
+          <SiTypescript />
         </div>
       </div>
     </footer>
