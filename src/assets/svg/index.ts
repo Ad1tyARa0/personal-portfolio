878d1e20253bd2a98e22440345b094cf017ebc53
Aditya S Rao <aditya.s.rao12@gmail.com>
2023-10-27 20:00:18 +0530

    Added services page.

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,8 @@
 import gatsby from "./gatsby.svg";
 
+import development from "./development.svg";
+import uiUx from "./ui_ux.svg";
+
 export const gatsbyLogo = gatsby;
+export const developmentIllustration = development;
+export const uiUxIllustration = uiUx;
--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 // SCSS.
-import "./services.scss";
+import "./Services.scss";
 import { useSectionInView } from "../../hooks/useIntersectionObserver";
+
+import { developmentIllustration, uiUxIllustration } from "../../assets/svg";
+import { Heading } from "../common/heading/Heading";
+import { useThemeContext } from "../../context/ThemeContext";
 
 const css_prefix = "c--se__";
 
@@ -12,9 +16,51 @@ interface ServicesProps {}
 const ServicesComponent: React.FunctionComponent<ServicesProps> = () => {
   const { ref } = useSectionInView("Services");
 
+  const { theme } = useThemeContext();
+
+  const illustrations = [
+    {
+      id: 1,
+      img: developmentIllustration,
+      label: "Web & Mobile Development",
+    },
+
+    {
+      id: 2,
+      img: uiUxIllustration,
+      label: "UX UI",
+    },
+  ];
+
   return (
     <div className={`${css_prefix}main`} ref={ref} id="Services">
-      <div>services</div>
+      {illustrations.map(e => {
+        return (
+          <div className={`${css_prefix}container`}>
+            <div className={`${css_prefix}service-label`}>
+              <Heading variant="subheading2" text={e.label} />
+            </div>
+
+            <div
+              key={e.id}
+              className={`${css_prefix}service-item-main ${
+                theme === "dark" ? css_prefix + "service-item-main-dark" : ""
+              }`}
+            >
+              <img
+                className={`${css_prefix}service-image`}
+                src={e.img}
+                alt={e.label}
+              />
+            </div>
+          </div>
+        );
+      })}
+
+      {/* <img
+        src={webDevelopmentIllustration}
+        className={`${css_prefix}service-image`}
+      /> */}
     </div>
   );
 };
